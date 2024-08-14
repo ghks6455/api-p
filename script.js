2ab2fd01df25187478d8845e1d317a9937cab3e6
ghks6455 <ghks6455@gmail.com>
2024-08-14 16:42:40 +0900

    first commit

--- script.js
+++ script.js
@@ -3,21 +3,28 @@ $(document).ready(() => {
   fetch("https://jsonplaceholder.typicode.com/posts")
     .then((response) => response.json())
     .then((json) => {
-      // console.log(json.id);
       // html 태그에 데이터 넣기
       json.forEach((post) => {
-        console.log(post.title);
-        console.log(post.body);
-
         // 모든 포스트를 담을 요소에 추가
         $("#allPost").append(`
           <div id="post">
             <h2>${post.title}</h2>
             <p>${post.body}</p>
+            <p>id: ${post.id}</p>
           </div>
         `);
       });
+      $("#a").click((id) => {
+        window.location.href = `detail.html?id=${id.id}`;
+        console.log(id);
+      });
     });
+  $(`#search`).click(() => {
+    let searchPlaceEl = $("#searchPlace").val();
+    // console.log(searchPlaceEl);
+    $("h2").parent().hide();
+    $(`h2:contains(${searchPlaceEl})`).parent().show();
+  });
 });
 
 // Object.keys(json).forEach((post) => {
